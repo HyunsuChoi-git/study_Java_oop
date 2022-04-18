dee9af7fbb5717d9275d8adc8156633ce6e640df
HyunsuChoi-git <choihs1054@gmail.com>
2022-04-18 17:30:31 +0900

    study_HelloWorld

---
+++ src/main/java/com/heracomp/study_Java_oop/Main.java
@@ -0,0 +1,7 @@
+package com.heracomp.study_Java_oop;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/test/java/com/heracomp/study_Java_oop/MainTest.java
@@ -0,0 +1,14 @@
+package com.heracomp.study_Java_oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main(){
+        Main.main(null);
+    }
+
+}
