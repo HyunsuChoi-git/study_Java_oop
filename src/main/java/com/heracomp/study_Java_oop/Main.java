e3094318c67056ddad2de05e1d2c951447724c0a
HyunsuChoi-git <choihs1054@gmail.com>
2022-04-19 15:02:41 +0900

    study_#5_DesignSortInterface
     Sort 인터페이스를 생성하여 각 Sort클래스에서 Sort인터페이스를 구현하도록 수정

--- src/main/java/com/heracomp/study_Java_oop/Main.java
+++ src/main/java/com/heracomp/study_Java_oop/Main.java
@@ -1,13 +1,14 @@
 package com.heracomp.study_Java_oop;
 
 import com.heracomp.study_Java_oop.logic.BubbleSort;
+import com.heracomp.study_Java_oop.logic.Sort;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new BubbleSort<>();
 
         System.out.println("[result] "+sort.sort(Arrays.asList(args)));
 
---
+++ src/main/java/com/heracomp/study_Java_oop/logic/Sort.java
@@ -0,0 +1,7 @@
+package com.heracomp.study_Java_oop.logic;
+
+import java.util.List;
+
+public interface Sort <T extends Comparable<T>>{
+    List<T> sort(List<T> list);
+}
